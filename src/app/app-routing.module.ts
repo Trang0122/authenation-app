8a25268ae2a99033608baec2f3c50f7f17373af1
Trang0211 <trangptt.bhsoft@gmail.com>
2020-05-25 17:00:27 +0700

    delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
-import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 import { ErrorComponent } from './components/error/error.component';
 import { WellcomeComponent } from './components/wellcome/wellcome.component';
-import { ConfigComponent } from './config/config/config.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
@@ -15,10 +12,6 @@ const routes: Routes = [
     path:"",
     redirectTo : 'home',
     pathMatch : 'full',
-  },
-  {
-    path: "product",
-    loadChildren : () => import('./components/product/product.module').then(m => m.ProductModule),
   },
   {
     path: "home",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
-import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 import { ErrorComponent } from './components/error/error.component';
 import { WellcomeComponent } from './components/wellcome/wellcome.component';
-import { HereListComponent } from './components/here-list/here-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ConfigComponent } from './config/config/config.component';
-import { ConfigService } from './config/config.service';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { AccountService } from './services/account.service';
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent,
-    ReactiveFormComponent,
     ErrorComponent,
     WellcomeComponent,
-    HereListComponent,
-    ConfigComponent,
     LoginComponent,
     SignupComponent,
-    HomeComponent,
-    ProductComponent,
-    ProductDetailComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +28,7 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
     HttpClientModule
     
   ],
-  providers: [ConfigService],
+  providers: [AccountService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
